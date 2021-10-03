cb19293dc3b8876ef088238f4ee3221d0dd93a44
Sajal Gupta <guptasajal411@gmail.com>
2021-10-03 11:33:12 +0530

    feat: :rocket: Add animations

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Card(props) {
     return (
-        <div className="card">
+        <div className="card" tabindex="1">
             <img src = {props.profilePicture} className="img" alt={props.name + "'s profile picture"} />
             <h2 className="name">{props.name}</h2>
             <em>{props.description}</em>
