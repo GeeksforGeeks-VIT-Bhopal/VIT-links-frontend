66d9c99a315aa78bf84d372aab10109f7b12a68c
Sajal Gupta <guptasajal411@gmail.com>
2021-10-03 11:23:59 +0530

    feat: :rocket: Add stylesheet

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,72 @@
 import React from 'react';
-import Card from './Card'
+import Card from './Card';
+import "./styles.css";
 
 function App(){
     return(
         <div>
-            <Card name="Sajal"
-            profilePicture="https://github.com/guptasajal411.png"
-            description="Full Stack Web Developer | Web Lead @ GeeksforGeeks"
-            university="VIT Bhopal"
-            branch="Computer Science & Engineering"
-            graduationYear="2024" />
+            <div className="cardWrapper">
+                <Card name="Sajal"
+                profilePicture="https://github.com/guptasajal411.png"
+                description="Full Stack Web Developer | Web Lead @ GeeksforGeeks"
+                university="VIT Bhopal"
+                branch="Computer Science & Engineering"
+                graduationYear="2024" />
+                <Card name="Sajal"
+                profilePicture="https://github.com/guptasajal411.png"
+                description="Full Stack Web Developer | Web Lead @ GeeksforGeeks"
+                university="VIT Bhopal"
+                branch="Computer Science & Engineering"
+                graduationYear="2024" />
+                <Card name="Sajal"
+                profilePicture="https://github.com/guptasajal411.png"
+                description="Full Stack Web Developer | Web Lead @ GeeksforGeeks"
+                university="VIT Bhopal"
+                branch="Computer Science & Engineering"
+                graduationYear="2024" />
+                <Card name="Sajal"
+                profilePicture="https://github.com/guptasajal411.png"
+                description="Full Stack Web Developer | Web Lead @ GeeksforGeeks"
+                university="VIT Bhopal"
+                branch="Computer Science & Engineering"
+                graduationYear="2024" />
+                <Card name="Sajal"
+                profilePicture="https://github.com/guptasajal411.png"
+                description="Full Stack Web Developer | Web Lead @ GeeksforGeeks"
+                university="VIT Bhopal"
+                branch="Computer Science & Engineering"
+                graduationYear="2024" />
+                <Card name="Sajal"
+                profilePicture="https://github.com/guptasajal411.png"
+                description="Full Stack Web Developer | Web Lead @ GeeksforGeeks"
+                university="VIT Bhopal"
+                branch="Computer Science & Engineering"
+                graduationYear="2024" />
+                <Card name="Sajal"
+                profilePicture="https://github.com/guptasajal411.png"
+                description="Full Stack Web Developer | Web Lead @ GeeksforGeeks"
+                university="VIT Bhopal"
+                branch="Computer Science & Engineering"
+                graduationYear="2024" />
+                <Card name="Sajal"
+                profilePicture="https://github.com/guptasajal411.png"
+                description="Full Stack Web Developer | Web Lead @ GeeksforGeeks"
+                university="VIT Bhopal"
+                branch="Computer Science & Engineering"
+                graduationYear="2024" />
+                <Card name="Sajal"
+                profilePicture="https://github.com/guptasajal411.png"
+                description="Full Stack Web Developer | Web Lead @ GeeksforGeeks"
+                university="VIT Bhopal"
+                branch="Computer Science & Engineering"
+                graduationYear="2024" />
+                <Card name="Sajal"
+                profilePicture="https://github.com/guptasajal411.png"
+                description="Full Stack Web Developer | Web Lead @ GeeksforGeeks"
+                university="VIT Bhopal"
+                branch="Computer Science & Engineering"
+                graduationYear="2024" />
+            </div>
         </div>
     );
 }
