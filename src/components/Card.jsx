79b12b03d2c20412f047c955678c859ba9cda1ee
Sajal Gupta <guptasajal411@gmail.com>
2021-10-10 11:52:27 +0530

    feat: :rocket: Add card styles

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
+import React from 'react';
+import "./Card.css";
 
 export default function Card(props) {
     console.log("hello")
     return (
         <div className="card" tabindex="1">
-            <img src = {props.profilePicture} className="img" alt={props.name + "'s profile picture"} />
-            <h2 className="name">{props.name}</h2>
-            <em>{props.description}</em>
-            <p>{props.university + ' ' + props.branch}</p>
-            <p>{props.graduationYear}</p>
-            <b>{props.githubUsername}</b>
-            <a href={"/" + props.githubUsername}>Go to individual profile</a>
+            <a href={"/" + props.githubUsername}>
+                <img src = {props.profilePicture} className="img" alt={props.name + "'s profile picture"} />
+                <h2 className="name">{props.name}</h2>
+                <em>{props.description}</em>
+                <p>{props.university + ' ' + props.branch}</p>
+                <p>{props.graduationYear}</p>
+                <a href={"/" + props.githubUsername}>
+                    <button>Go to {props.githubUsername + "'s"} profile</button>
+                </a>
+            </a>
         </div>
     );
 }
