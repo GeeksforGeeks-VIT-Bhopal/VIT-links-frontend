bcc0ee8963fd9a5aea76148b41e741b977714627
Divyansh-Singhal <divyansh.singhal2020@vitbhopal.ac.in>
2021-10-25 12:50:47 +0530

    Done Navbar changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ export default function Navbar() {
   
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div id="navflr" class="navbar-nav">
-           <a class="nav-item nav-link" href="#">Home</a>
+           <a class="nav-item nav-link" href="/">Home</a>
       <a class="nav-item nav-link" href="/about">About</a>
       <a class="nav-item nav-link" href="/contact">Contact</a>
      
